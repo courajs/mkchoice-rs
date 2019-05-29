df0c38a63dd2db108e44e92299321e2a6feb0053
Aaron Sikes <aaron@sikes.io>
2019-05-29 14:13:54 -0400

    Print a hardcoded list of options

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 use termion;
+use termion::{clear, cursor};
 use std::error::Error;
 use std::io::Write;
 
+
+// struct State { choices: list of strings, position: which one we point to }
+// method for State print()
+// make a hardcoded State and use print() in main
+
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut thing = termion::get_tty()?;
-    thing.write_all(b"hey!")?;
-    Ok(())
+    let mut tty = termion::get_tty()?;
+        tty.write_all(b"\n\n")?;
+        tty.write_all(b"world!\n")?;
+        
+        let msg = format!("{}hello...\n\n", cursor::Up(2));
+        tty.write_all(msg.as_bytes())?;
+
+        let o = Options {choices: vec!["one", "two"], position: 0};
+        let s = o.print();
+        tty.write_all(s.as_bytes())?;
+
+        Ok(())
 }
+
+struct Options<'a> {
+    choices: Vec<&'a str>,
+    position: usize
+}
+
+impl<'a> Options<'a> {
+    fn print(&self) -> String {
+
+        let mut options = self.choices.join("\n");
+        options.insert_str(0,"Choose one: \n");
+        return options
+    }
+}
+
+
