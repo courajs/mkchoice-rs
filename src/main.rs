d0d0660354e7da0fe26febd31784446f7ac724b1
Aaron Sikes <aaron@sikes.io>
2021-01-12 10:31:49 -0500

    Add selection via index rather than string match

--- src/main.rs
+++ src/main.rs
@@ -32,16 +32,18 @@ struct Args {
     vanish: bool,
     prompt: Option<String>,
     selection: Option<String>,
+    selected_index: Option<usize>,
     stdin_index: Option<usize>,
     choices: Vec<String>,
 }
 impl Args {
-    fn new(args: Vec<String>) -> Self {
+    fn parse(args: Vec<String>) -> Result<Self, std::num::ParseIntError> {
         let mut result = Self {
             help: false,
             vanish: false,
             prompt: None,
             selection: None,
+            selected_index: None,
             stdin_index: None,
             choices: Vec::new(),
         };
@@ -58,6 +60,11 @@ impl Args {
                 "--selection" | "-s" => {
                     if let Some((_,next)) = args.next() {
                         result.selection = Some(next);
+                    }
+                },
+                "--index" | "-n" => {
+                    if let Some((_,next)) = args.next() {
+                        result.selected_index = Some(usize::from_str_radix(&next, 10)?);
                     }
                 },
                 "-" => result.stdin_index = Some(i),
@@ -77,14 +84,33 @@ impl Args {
                     arg.replace_range(.."--selection=".len(), "");
                     result.selection = Some(arg);
                 },
+                s if s.starts_with("-n=") => {
+                    arg.replace_range(..3, "");
+                    result.selected_index = Some(usize::from_str_radix(&arg, 10)?);
+                },
+                s if s.starts_with("--index=") => {
+                    arg.replace_range(.."--index=".len(), "");
+                    result.selected_index = Some(usize::from_str_radix(&arg, 10)?);
+                },
                 "--" => break,
                 _ => result.choices.push(arg),
             }
         }
         result.choices.extend(args.map(|(_,s)|s));
 
-        result
+        Ok(result)
     }
+    // fn into_chooser(self, stdin: impl Read) -> Chooser {
+    //     let mut result = Chooser {
+    //         vanish: self.vanish,
+    //         prompt: self.prompt.unwrap_or_else(||String::from("Choose one:")),
+    //         choices: self.choices,
+    //         curent_choice: 0,
+    //     };
+    //     if Some(index) = self.stdin_index {
+
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -93,14 +119,21 @@ mod tests {
 
     #[test]
     fn test_arg_parse() {
-        let args = vec!["-s=master", "a", "-", "--vanish", "b", "-p", "Which one?", "--", "-p", "-h", "-", "--", "master", "z"].into_iter().map(String::from).collect();
-        assert_eq!(Args::new(args), Args {
+        let args = vec!["-s=master", "a", "-", "--vanish", "b", "-p", "Which one?", "-n", "12", "--", "-p", "-h", "-", "--", "master", "z"].into_iter().map(String::from).collect();
+        assert_eq!(Args::parse(args), Ok(Args {
             help: false,
             vanish: true,
             prompt: Some("Which one?".to_string()),
             selection: Some("master".to_string()),
+            selected_index: Some(12),
             stdin_index: Some(2),
             choices: vec!["a", "b", "-p", "-h", "-", "--", "master", "z"].into_iter().map(String::from).collect(),
-        });
+        }));
+    }
+
+    #[test]
+    fn test_index_parse_error() {
+        let args = vec!["--index", "abc"].into_iter().map(String::from).collect();
+        assert_eq!(Args::parse(args).unwrap_err(), usize::from_str_radix("abc", 10).unwrap_err());
     }
 }
